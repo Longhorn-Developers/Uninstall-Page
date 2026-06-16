1c9c5ab674aced6e5603d0ee151a651c4af82a6c
Derek <derex1987@gmail.com>
2026-06-16 18:27:57 -0500

    style: spacing

---
+++ src/shared/types/Spacing.ts
@@ -0,0 +1,11 @@
+export const spacing = {
+  'spacing-3': '0.75rem',
+  'spacing-5': '1.25rem',
+  '4.5': '1.125rem',
+  '3': '0.75rem',
+  '5': '1.25rem',
+  '6': '1.5rem',
+  '8': '2rem',
+} as const;
+
+export type Spacing = typeof spacing;
